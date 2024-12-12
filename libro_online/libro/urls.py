62d16008c72a4f70bdea1b153e05974275d55ae1
javierEstudios <xjfaravi975@ieshnosmachado.org>
2024-12-12 21:03:02 +0100

    Se me olvido la url de eliminar libro

--- libro_online/libro/urls.py
+++ libro_online/libro/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import pagina_principal, lista_libros, detalles_libro, crear_libro, editar_libro, lista_autores, detalles_autor, detalles_lector, leer_capitulo
+from .views import pagina_principal, lista_libros, detalles_libro, crear_libro, editar_libro, eliminar_libro, lista_autores, detalles_autor, detalles_lector, leer_capitulo
 
 urlpatterns = [
     path("", pagina_principal.as_view(), name="pagina_de_inicio"),
@@ -7,6 +7,7 @@ urlpatterns = [
     path("libro/<int:pk>", detalles_libro.as_view(), name="libro"),
     path("libro/nuevo", crear_libro.as_view(), name="nuevo_libro"),
     path("libro/editar/<int:pk>", editar_libro.as_view(), name="editar_el_libro"),
+    path("libro/eliminar/<int:pk>", eliminar_libro.as_view(), name="eliminar_el_libro"),
     path("autores/", lista_autores.as_view(), name="lista_de_autores"),
     path("autor/<int:pk>", detalles_autor.as_view(), name="autor"),
     path("capitulo/<int:pk>", leer_capitulo.as_view(), name="capitulo"),
